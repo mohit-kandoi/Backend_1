912d858cee9c81aa47611c708e531a5342337e1c
mohit-kandoi <mohitkandoi65@gmail.com>
2024-02-08 14:22:48 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-const app = express();
+// This code sets up an Express.js application with various middleware to handle HTTP requests and responses.
 
+import express from "express";
+// express: This imports the Express.js framework, which simplifies the process of building web applications and APIs in Node.js
+import cors from "cors";
+// cors: This imports the CORS (Cross-Origin Resource Sharing) middleware, which allows control over how resources on a web page can be requested from another domain.
+import cookieParser from "cookie-parser";
+// cookieParser: This imports the middleware used for parsing cookies in the incoming requests.
+
+const app = express();
+// Initializes an Express application by calling 'express()'
+
+// cors: The CORS middleware is applied using app.use(). It is configured to allow requests from origins specified in the CORS_ORIGIN environment variable, with credentials enabled (credentials: true). This means the server can receive requests from other origins and include cookies or HTTP authentication information in the request.
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -6,6 +6,19 @@ const asyncHandler = (requestHandler) => {
 
 export { asyncHandler };
 
+// This code defines a higher-order function called asyncHandler that wraps around other route handler functions in an Express.js application. Let's break it down:
+
+// Function Definition:
+// const asyncHandler = (requestHandler) => { ... };: This defines a function named asyncHandler. It takes a requestHandler function as its argument.
+
+// Inner Function:
+// (req, res, next) => { ... }: This is an arrow function that acts as the inner function of asyncHandler. It takes three parameters: req (the request object), res (the response object), and next (a callback function to pass control to the next middleware function).
+// This inner function serves as a wrapper around the requestHandler function provided to asyncHandler.
+
+// Asynchronous Handling:
+// Promise.resolve(requestHandler(req, res, next)): This line invokes the requestHandler function with the provided req, res, and next parameters. It returns a promise.
+// .catch((err) => next(err)): This attaches a .catch() block to the promise. If the promise rejects (i.e., if an error occurs during the execution of the requestHandler function), it calls next(err), passing the error object to the next middleware or error handling route.
+
 // const asyncHandler = (fn) => async (req, res, next) => {
 //   try {
 //     await fn(req, res, next);
