86f178f9c60950e2afda31df356733063e50dcca
mohit-kandoi <mohitkandoi65@gmail.com>
2024-02-08 12:10:26 +0530

    added comments

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
+//Function connectDB:
+// This is an asynchronous function defined using the async keyword. It's responsible for establishing a connection to the MongoDB database.
+// Inside the function, a try-catch block is used to handle potential errors that might occur during the connection process.
+
+// Connection to MongoDB:
+// mongoose.connect() is called within the try block to establish a connection to the MongoDB database. It uses the MONGODB_URI environment variable provided through process.env and appends the database name from the DB_NAME constant.
+// The connection is "awaited" since mongoose.connect() returns a promise. Once the connection is established, a connectionInstance object is returned.
+// A log message is printed indicating that the MongoDB connection was successful. It displays the host of the MongoDB connection.
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
@@ -15,3 +23,6 @@ const connectDB = async () => {
   }
 };
 export default connectDB;
+
+// Export:
+// The connectDB function is exported as the default export, allowing it to be imported and used in other parts of the application.
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
+// dotenv: This package loads environment variables from a .env file into process.env.
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
@@ -7,6 +8,10 @@ import connectDB from "./db/index.js";
 dotenv.config({
   path: "./env",
 });
+
+// connectDB();
+// Getting PROMISE from this Function(If the connection is successful, the server starts listening on a specified port (either the one provided in the environment variable process.env.PORT or port 8000 if not specified). A message is logged indicating that the server is running and on which port.
+// If the connection fails, an error message is logged indicating that the MongoDB connection failed, along with the error object. )
 
 connectDB()
   .then(() => {
@@ -17,6 +22,8 @@ connectDB()
   .catch((err) => {
     console.log("MongoDB connection failed!!", err);
   });
+
+// Overall, this code sets up a server to run an application, connects it to a MongoDB database, and utilizes environment variables for configuration, including the port number.
 
 /*
 import express from "express";
